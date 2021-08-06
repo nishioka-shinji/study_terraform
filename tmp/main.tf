49acb02eff2ba2ebcba269611a616651da71bfaa
nishioka-shinji <s61nov11.shinji.nishioka@gmail.com>
2021-08-07 07:52:12 +0900

    for_each確認

--- tmp/main.tf
+++ tmp/main.tf
@@ -13,7 +13,23 @@ provider "aws" {
   region  = "ap-northeast-1"
 }
 
-resource "aws_iam_user" "user" {
-  count = 2
-  name  = "testuser-${count.index}"
+# resource "aws_iam_user" "user" {
+#   count = 2
+#   name  = "testuser-${count.index}"
+# }
+
+resource "aws_vpc" "vpc" {
+  cidr_block = "192.168.0.0/20"
+}
+
+resource "aws_subnet" "subnet" {
+  for_each = {
+    "192.168.1.0/24" = "ap-northeast-1a"
+    "192.168.2.0/24" = "ap-northeast-1c"
+    "192.168.3.0/24" = "ap-northeast-1d"
+  }
+
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = each.key
+  availability_zone = each.value
 }
