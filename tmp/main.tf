03f94a4b57f1b09ca974e53628c9de2b6cb636df
nishioka-shinji <s61nov11.shinji.nishioka@gmail.com>
2021-08-08 07:02:06 +0900

    セクション19

--- tmp/main.tf
+++ tmp/main.tf
@@ -13,11 +13,14 @@ provider "aws" {
   region  = "ap-northeast-1"
 }
 
-# resource "aws_iam_user" "user" {
-#   count = 2
-#   name  = "testuser-${count.index}"
-# }
+/* study "count"
+resource "aws_iam_user" "user" {
+  count = 2
+  name  = "testuser-${count.index}"
+}
+*/
 
+/* study "for_each"
 resource "aws_vpc" "vpc" {
   cidr_block = "192.168.0.0/20"
 }
@@ -33,3 +36,66 @@ resource "aws_subnet" "subnet" {
   cidr_block        = each.key
   availability_zone = each.value
 }
+*/
+
+/* study "for"
+variable "obj" {
+  type = object({
+    name = string
+    age  = number
+  })
+  default = {
+    name = "tanaka"
+    age  = 28
+  }
+}
+
+variable "tuple" {
+  type = tuple([
+    string, number
+  ])
+  default = ["tanaka", 28]
+}
+
+variable "list" {
+  type    = list(string)
+  default = ["tanaka", "sato"]
+}
+
+variable "map" {
+  type = map(string)
+  default = {
+    "High" = "m5.2xlarge"
+    "Mid"  = "m5.large"
+    "Low"  = "t2.micro"
+  }
+}
+
+variable "set" {
+  type = set(string)
+  default = [
+    "tanaka",
+    "sato",
+    "tanaka",
+    "sato"
+  ]
+}
+*/
+
+/* 以下をterraform consoleで実行
+[ for s in var.list : upper(s) ]
+{ for s in var.list : s => upper(s) }
+[ for k,v in var.map : upper(v) ]
+{ for k,v in var.map : v => k }
+*/
+
+/* study 三項演算子
+variable "message" {
+  type = string
+  default = ""
+}
+*/
+
+/* 以下をterraform consoleで実行
+var.message != "" ? var.message : "Good night"
+*/
